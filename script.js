e53ef3f66b3c72a27c7e09aa2b2bc7908a780294
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2025-01-24 16:01:28 +0530

    style&structure, animate&sound->click randomColor
    The basic structure and styling is done. script for click animation and sound\/
    randomColor to choose and start playing game

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
-
 let gamePattern = [];
+
+let userClickedPattern = [];
 // $('h1').css('color', 'red')
-let buttonColors = ['red', 'blue', 'green', 'yellow'];
-let randomChoosenColor = nextSequence();
+let buttonColors = ["red", "blue", "green", "yellow"];
+let randomChoosenColor = buttonColors[nextSequence()];
 
 gamePattern.push(randomChoosenColor);
 
-function nextSequence(){
-    let random = Math.floor(Math.random() * 4);
+let colorID = `#${randomChoosenColor}`;
+
+
+$('.button').on('click', function () {
+  // console.log();
+
+  let buttonClicked = $(this).attr('id');
+
+  // console.log(buttonClicked);
+
+  userClickedPattern.push(buttonClicked);
+  console.log(userClickedPattern);
+
+  let audio = new Audio(`assets/sounds/${buttonClicked}.mp3`)
+
+  audio.play();
+
+  $(this).toggleClass('pressed');
+
+  setTimeout(() => {
+    $(this).toggleClass('pressed');
+  }, 150)
+
+});
+
+
+
+
+// console.log(randomChoosenColor);
+// $(colorID).toggleClass('pressed');
+// console.log(randomChoosenColor);
+// let soundSrc = `assets/sounds/${randomChoosenColor}.mp3`;
+// let audio = new Audio(soundSrc);
+
+// audio.play();
+// setTimeout(() => {
+//     $(colorID).toggleClass('pressed');
+// }, 300)
+
+// setTimeout(() => {
+//   $(randomChoosenColor).fadeIn();
+//   $(randomChoosenColor).fadeOut();
+// }, 500);
+
+function nextSequence() {
+  let random = Math.floor(Math.random() * 4);
+  return random;
 }
