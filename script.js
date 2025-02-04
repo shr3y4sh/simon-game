2bfeee3b95fb4a4d5b133a03c32a3f4ec3aba2a8
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2025-02-04 14:10:53 +0530

    add game start, button flashing, check
    pressing a key, start a game, display level as heading
    button flash after a while, click on the button store it
    check if user pattern == game pattern

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 let gamePattern = [];
+let buttonColors = ["red", "blue", "green", "yellow"];
 let level = 0;
+let randomChoosenColor;
 let userClickedPattern = [];
-
+let started = false;
 ////////////////////////////////////////////////////////
 
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
   $("#info-bar").html(`Level ${level}`);
 
-  level++;
   let random = Math.floor(Math.random() * 4);
 
   return random;
 }
 
 ///////////////////////////////////////////////////////////////
-// KEYPRESS EVENT
+// KEYPRESS EVENT ON TIME
 
 $("body").one("keypress", function () {
   // e.stopImmediatePropagation();
-
-  console.log("whattup");
-  nextSequence();
+  randomChoosenColor = buttonColors[nextSequence()];
+  console.log(randomChoosenColor);
+  gamePattern.push(randomChoosenColor);
+  setTimeout(() => {
+    buttonFlashing(randomChoosenColor);
+  }, 500);
+  console.log(level, gamePattern);
 });
 
 //////////////////////////////////////////////////////////////////
-let buttonColors = ["red", "blue", "green", "yellow"];
 
-var randomChoosenColor =
-  buttonColors[
-    () => {
-      return nextSequence();
-    }
-  ];
+// try {
+//   var randomChoosenColor = buttonColors[nextSequence()];
+// } catch {
+//   console.log("nextSequence not called");
+// }
+// gamePattern.push(randomChoosenColor);
 
-gamePattern.push(randomChoosenColor);
-
-let colorID = `#${randomChoosenColor}`;
+// let colorID = `#${randomChoosenColor}`;
 
 ///////////////////////////////////////////////////////////
 // BUTTON CLICK EVENT
@@ -54,4 +58,37 @@ $(".button").on("click", function () {
   setTimeout(() => {
     $(this).toggleClass("pressed");
   }, 150);
+
+  console.log(userClickedPattern);
+
+  checkAnswer(level);
 });
+
+///////////////////////////////////////////////////////////
+
+function buttonFlashing(buttonID) {
+  let button = $(`#${buttonID}`);
+  let audio = new Audio(`assets/sounds/${buttonID}.mp3`);
+
+  audio.play();
+
+  $(button).toggleClass("pressed");
+
+  setTimeout(() => {
+    $(button).toggleClass("pressed");
+  }, 150);
+}
+
+//////////////////////////////////////////////////////////////
+// USER CHECK ANSWER
+
+function checkAnswer(currentLevel) {
+
+  for (let i = 0; i < currentLevel; i++) {
+    if (gamePattern[i] === userClickedPattern[i]) {
+      console.log("Success");
+    } else {
+      console.log("Wrong");
+    }
+  }
+}
